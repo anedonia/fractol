f380b82ea1365b182db25996f89c29617d779071
Louis DEVY <ldevy@e2r12p20.clusters.42paris.fr>
2022-03-21 22:08:17 +0100

    debut du mandelbrot

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: ldevy <ldevy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 16:33:48 by ldevy             #+#    #+#             */
-/*   Updated: 2022/03/16 15:09:05 by ldevy            ###   ########.fr       */
+/*   Updated: 2022/03/21 22:00:20 by ldevy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,29 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdarg.h>
+# include <math.h>
 
 typedef	struct s_data
 {
 	void *img;
 	void *mlx;
 	void *mlx_win;
-	char type;
+	char *addr;
+	int type;
 	int bits_per_pixel;
 	int line_length;
 	int endian;
+	int ite_max;
+	int x;
+	int y;
+	float c_r;
+	float c_i;
+	float x_cadre;
+	float y_cadre;
+	int zoom;
+	
+	
+	
 }	t_data;
 
 
--- srcs/fisrt/calc.c
+++ srcs/fisrt/calc.c
@@ -1,34 +1,46 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   first.c                                            :+:      :+:    :+:   */
+/*   calc.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldevy <ldevy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/16 14:27:04 by ldevy             #+#    #+#             */
-/*   Updated: 2022/03/21 21:32:07 by ldevy            ###   ########.fr       */
+/*   Created: 2022/03/21 15:54:15 by ldevy             #+#    #+#             */
+/*   Updated: 2022/03/21 22:06:20 by ldevy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../fractol.h"
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
+//void une ft qui choisie le type de set 
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+void ft_calc(t_data *set)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (set->x = i < 400)
+	{
+		j = 0;
+		while (set->y = j < 400)
+		{
+			ft_mandelbrot(&set);
+			j++;
+		}
+		i++;
+	}
 }
 
-int	game(t_data *set)
+void	mandelbrot_init(t_data *set)
 {
-	set->mlx = mlx_init();
-	
-	set->mlx_win = mlx_new_window(set->mlx,400, 400, set->type);
-	ft_calc(&set);
-	//hook
-	//hook de fermeture
-	mlx_loop(set->mlx);
-	
-	return (1);
+	set->x_cadre = -2;
+	set->y_cadre = -2;
+	set->ite_max = 50;
+	set->zoom = 200;
+}
+
+int	ft_mandelbrot(t_data *set)
+{
+	set->		
 }
